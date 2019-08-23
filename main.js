237a45dcf48837c05959d67b8702f3bba001006d
ritik09 <ritikgupta0912@gmail.com>
2019-08-23 12:23:39 +0530

    pocket tanks

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-function initCanvas(){
+function inCanvas(){
     var ctx = document.getElementById('my_canvas').getContext('2d');
     var cW = ctx.canvas.width, cH = ctx.canvas.height;
-    var y = 0;var x=0;
+    var y = 0;var x=0;var z=y;
     function animate(){
-        ctx.save();
+        
         ctx.clearRect(0, 0, cW, cH);
         ctx.fillStyle= "blue";
         ctx.fillRect(0, y, 50, 50);
         y++;
         ctx.fillStyle="magenta";
         ctx.fillRect(x,0,50,50);
-        x=x+2;
-        if(x==450)
-            y+=1;
-        if(y==250)
-            x+=1;
-        ctx.restore();
+        x++;
+        ctx.fillStyle= "yellow";
+        ctx.fillRect(350, 0, 20, 30);
+        ctx.fillStyle= "green";
+        ctx.fillRect(0, 350, 30, 20);
+        if(x==300)
+        {
+            x=0;
+        }
+        if(y==300)
+        {
+            y=0;
+        }
+        
+
     }
-    var animateInterval=setInterval(animate,30);
+    var animateInterval=setInterval(animate,25);
 }
 window.addEventListener('load',function(){
-initCanvas();
+inCanvas();
 });
